a7bb8310f3788e4f904c22f032b48b34b3c5a39d
OEmiliatanO <emiliaistruelove@gmail.com>
2024-04-15 05:47:38 +0800

    implement APSO

---
+++ APSO/plot.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+make record all
+
+if [ "$1" == "Ackley" ] || [ "$1" == "1" ]
+then
+    echo "Ackley"
+    ./main.elf --N 1000 --D 2 --k 100 --alpha 0.15 --beta 0.5 --L 1 --test_function 1  > /dev/null
+    python plot.py 1
+elif [ "$1" == "Rastrigin" ] || [ "$1" == "2" ]
+then 
+    echo "Rastrigin"
+    ./main.elf --N 1000 --D 2 --k 100 --alpha 0.15 --beta 0.5 --L 1 --test_function 2  > /dev/null
+    python plot.py 2
+elif [ "$1" == "HappyCat" ] || [ "$1" == "3" ]
+then
+    echo "HappyCat"
+    ./main.elf --N 1000 --D 2 --k 100 --alpha 0.15 --beta 0.5 --L 1 --test_function 3  > /dev/null
+    python plot.py 3
+elif [ "$1" == "Rosenbrock" ] || [ "$1" == "4" ]
+then
+    echo "Rosenbrock"
+    ./main.elf --N 1000 --D 2 --k 100 --alpha 0.15 --beta 0.5 --L 1 --test_function 4  > /dev/null
+    python plot.py 4
+elif [ "$1" == "Zakharov" ] || [ "$1" == "5" ]
+then
+    echo "Zakharov"
+    ./main.elf --N 1000 --D 2 --k 100 --alpha 0.15 --beta 0.5 --L 1 --test_function 5  > /dev/null
+    python plot.py 5
+elif [ "$1" == "Michalewicz" ] || [ "$1" == "6" ]
+then
+    echo "Michalewicz"
+    ./main.elf --N 1000 --D 2 --k 100 --alpha 0.15 --beta 0.5 --L 1 --test_function 6  > /dev/null
+    python plot.py 6
+elif [ "$1" == "all" ] || [ "$1" == "-1" ]
+then
+    echo "Ackley"
+    ./main.elf --N 1000 --D 2 --k 100 --alpha 0.15 --beta 0.5 --L 1 --test_function 1  > /dev/null
+    python plot.py 1
+    
+    echo "Rastrigin"
+    ./main.elf --N 1000 --D 2 --k 100 --alpha 0.15 --beta 0.5 --L 1 --test_function 2  > /dev/null
+    python plot.py 2
+    
+    echo "HappyCat"
+    ./main.elf --N 1000 --D 2 --k 100 --alpha 0.15 --beta 0.5 --L 1 --test_function 3  > /dev/null
+    python plot.py 3
+    
+    echo "Rosenbrock"
+    ./main.elf --N 1000 --D 2 --k 100 --alpha 0.15 --beta 0.5 --L 1 --test_function 4  > /dev/null
+    python plot.py 4
+    
+    echo "Zakharov"
+    ./main.elf --N 1000 --D 2 --k 100 --alpha 0.15 --beta 0.5 --L 1 --test_function 5  > /dev/null
+    python plot.py 5
+
+    echo "Michalewicz"
+    ./main.elf --N 1000 --D 2 --k 100 --alpha 0.15 --beta 0.5 --L 1 --test_function 6  > /dev/null
+    python plot.py 6
+fi
