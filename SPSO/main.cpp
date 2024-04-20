221abcb59b39c939c23dea4de8e7f05247e465a3
OEmiliatanO <emiliaistruelove@gmail.com>
2024-04-21 04:51:53 +0800

    update standard PSO

---
+++ SPSO/main.cpp
@@ -0,0 +1,204 @@
+#include <iostream>
+#include <format>
+#include <fstream>
+#include <cmath>
+#include <chrono>
+#include <random>
+
+#include <vector.h>
+#include <test_function.h>
+
+double (*test_func)(const CH::vector<double>& X);
+
+std::string table[] = {"0", "Ackley", "Rastrigin", "HappyCat", "Rosenbrock", "Zakharov", "Michalewicz"};
+
+double PSO(size_t N, size_t D, size_t k, double vmax, double c1, double c2, double xl, double xu, double vl = 0.001, double vu = 1,[[maybe_unused]] int fn = 1)
+{
+    /*
+     N D k
+     x11 x12 x13 ... x1D  --\
+     v11 v12 v13 ... v1D    |
+     ...                    | <--- 0
+     xN1 xN2 xN3 ... xND    |
+     vN1 vN2 vN3 ... vND  --/
+     x11 x12 x13 ... x1D  --\
+     v11 v12 v13 ... v1D    |
+     ...                    | <--- 1
+     xN1 xN2 xN3 ... xND    |
+     vN1 vN2 vN3 ... vND  --/
+
+     ...
+     
+     x11 x12 x13 ... x1D  --\
+     v11 v12 v13 ... v1D    |
+     ...                    | <--- k
+     xN1 xN2 xN3 ... xND    |
+     vN1 vN2 vN3 ... vND  --/
+     */
+#ifdef SAVE
+    std::fstream fs;
+    fs.open(std::format("plot/{}/{}_path.txt", table[fn], table[fn]), std::ios::out);
+    fs << std::format("{} {} {}", N, D, k) << std::endl;
+#endif
+
+    constexpr double omega_max = 0.9, omega_min = 0.4;
+
+    CH::vector<double> X[N], V[N];
+    CH::vector<double> pbest[N];
+    double fpbest[N];
+    CH::vector<double> gbest;
+    double fgbest;
+
+    /*  init  */
+    std::random_device rnd_device;
+    std::mt19937 eng { rnd_device() };
+    std::uniform_real_distribution<double> Xdist {xl, xu};
+    std::uniform_real_distribution<double> Vdist {vl, vu};
+    std::uniform_real_distribution<double> dist01 {0, 1};
+
+    fgbest = std::numeric_limits<double>::max();
+    for (size_t i = 0; i < N; ++i)
+    {
+        X[i].resize(D);
+        V[i].resize(D);
+        std::generate(X[i].begin(), X[i].end(), [&] { return Xdist(eng); });
+        std::generate(V[i].begin(), V[i].end(), [&] { return Vdist(eng); });
+
+#ifdef SAVE
+        fs << X[i] << '\n' << V[i] << std::endl;
+#endif
+
+        pbest[i] = X[i];
+        fpbest[i] = test_func(X[i]);
+        if (fgbest > fpbest[i])
+        {
+            gbest = X[i];
+            fgbest = fpbest[i];
+        }
+    }
+    /*  init  */
+
+    double omega = omega_max;
+
+    /*  iteration */
+    for (size_t iter = 0; iter < k; ++iter)
+    {
+        CH::vector<double> r1, r2;
+        r1.resize(D); r2.resize(D);
+        std::generate(r1.begin(), r1.end(), [&] { return dist01(eng); });
+        std::generate(r2.begin(), r2.end(), [&] { return dist01(eng); });
+        for (size_t i = 0; i < N; ++i)
+        {
+            V[i] = omega * V[i] + c1 * CH::Hadamard_prod(r1, (pbest[i] - X[i])) + c2 * CH::Hadamard_prod(r2, (gbest - X[i]));
+            std::transform(V[i].begin(), V[i].end(), V[i].begin(), [&] (const auto& v) { return std::min(vmax, v); });
+            X[i] = X[i] + V[i];
+            std::transform(X[i].begin(), X[i].end(), X[i].begin(), [&] (const auto& x) { return std::max(xl, std::min(xu, x)); });
+            auto fxi = test_func(X[i]);
+            if (fxi < fpbest[i])
+            {
+                fpbest[i] = fxi;
+                pbest[i] = X[i];
+            }
+            if (fxi < fgbest)
+            {
+                fgbest = fxi;
+                gbest = X[i];
+            }
+#ifdef SAVE
+            fs << X[i] << '\n' << V[i] << std::endl;
+#endif
+        }
+
+        omega = omega_max - (omega_max - omega_min) * iter / k;
+    }
+    /*  iteration */
+#ifdef SAVE
+    fs.close();
+#endif
+    std::cout << fgbest << std::endl;
+    return fgbest;
+}
+
+// --N particle_num
+// --D dimension
+// --k iteration_num
+// --vmax vmax
+// --c1 c1
+// --c2 c1
+// --test_function fn
+// (k+1)*N <= D*10^4
+// ./main.elf --N 10 --D 10 --k 1 --vmax 1 --c1 2 --c2 0.2 --test_function Ackley
+int main([[maybe_unused]]int argc, char **argv)
+{
+    using namespace std::chrono_literals;
+
+    int N, D, k, fn;
+    double vmax, c1, c2;
+
+    sscanf(argv[2], "%d", &N);
+    sscanf(argv[4], "%d", &D);
+    sscanf(argv[6], "%d", &k);
+    sscanf(argv[8], "%lf", &vmax);
+    sscanf(argv[10], "%lf", &c1);
+    sscanf(argv[12], "%lf", &c2);
+    sscanf(argv[14], "%d", &fn);
+
+    switch(fn)
+    {
+        case 1:
+            test_func = static_cast<double (*)(const CH::vector<double>&)>(&Ackley);
+            break;
+
+        case 2:
+            test_func = static_cast<double (*)(const CH::vector<double>&)>(&Rastrigin);
+            break;
+
+        case 3:
+            test_func = static_cast<double (*)(const CH::vector<double>&)>(HappyCat);
+            break;
+
+        case 4:
+            test_func = static_cast<double (*)(const CH::vector<double>&)>(Rosenbrock);
+            break;
+
+        case 5:
+            test_func = static_cast<double (*)(const CH::vector<double>&)>(Zakharov);
+            break;
+
+        case 6:
+            test_func = static_cast<double (*)(const CH::vector<double>&)>(Michalewicz);
+            break;
+    }
+
+    N = std::min(N, D * 10000 / (k+1));
+    std::cerr << std::format("N = {}, D = {}, k = {}", N, D, k) << std::endl;
+
+    //constexpr auto EVAL_N = 30;
+    auto eval_num = EVAL_N;
+    double mean = 0;
+    double std = 0;
+    std::chrono::duration<double> time_mean = 0ms;
+    while (eval_num--)
+    {
+        double xl = 0, xu = 30, vl = 0.1, vu=1;
+        set_search_bound(&xu, &xl, fn);
+        auto st = std::chrono::steady_clock::now();
+        auto tmp_res = PSO(N, D, k, vmax, c1, c2, xl, xu, vl, vu, fn);
+        auto ed = std::chrono::steady_clock::now();
+        time_mean += ed - st;
+
+        mean += tmp_res;
+        std += tmp_res * tmp_res;
+    }
+
+    mean /= EVAL_N;
+    time_mean /= EVAL_N;
+    std /= EVAL_N;
+    std -= mean * mean;
+    std = sqrt(std);
+    
+    //std::cout << std::format("mean: {:.4f}±{:.4f}", mean, std) << std::endl;
+
+    std::cerr << std::format("mean: {}±{}, avg time: {}", mean, std, time_mean) << std::endl;
+    return 0;
+}
---
+++ SPSO/plot.py
@@ -0,0 +1,87 @@
+import numpy as np
+from random import randint, random
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from src import test_function as tfn
+import sys
+
+func_str_table = {1:"Ackley", 2:"Rastrigin", 3:"HappyCat", 4:"Rosenbrock", 5:"Zakharov", 6:"Michalewicz"}
+search_range = {1:[-32.768, 32.768], 2:[-5.12, 5.12], 3:[-20.0, 20.0], 4:[-10.0, 10.0], 5:[-10.0, 10.0], 6:[0.0, np.pi]}
+
+fn = int(sys.argv[1])
+x = np.linspace(search_range[fn][0], search_range[fn][1], 100)
+y = np.linspace(search_range[fn][0], search_range[fn][1], 100)
+X, Y = np.meshgrid(x, y)
+Z = []
+for i in range(X.shape[0]):
+    for j in range(X.shape[1]):
+        Z.append(tfn.function_table[fn]([X[i, j],Y[i,j]]))
+Z = np.array(Z)
+Z = Z.reshape((X.shape[0], X.shape[1]))
+
+plt.figure()
+contour = plt.contour(X, Y, Z)
+#plt.clabel(contour, inline = True)
+plt.colorbar(contour, label=f'{func_str_table[fn]} Function Value')
+
+with open(f"plot/{func_str_table[fn]}/{func_str_table[fn]}_path.txt") as f:
+    N, D, k = [int(x) for x in f.readline().strip().split(' ')]
+    pX = [[] for i in range(N)]
+    pV = [[] for i in range(N)]
+    gbest = []
+    for it in range(k):
+        for i in range(N):
+            pX[i].append([float(x) for x in f.readline().strip().split(' ')])
+            pV[i].append([float(x) for x in f.readline().strip().split(' ')])
+        gbest.append(min([tfn.function_table[fn]([pX[i][it][0], pX[i][it][1]]) for i in range(N)]))
+
+    for i in range(10):
+        r = randint(0, 255)
+        g = randint(0, 255)
+        b = randint(0, 255)
+        c = "#{:02x}{:02x}{:02x}".format(r, g, b)
+        for it in range(k):
+            plt.scatter(pX[i][it][0], pX[i][it][1], c=c, s=3, zorder=5)
+            if it > 0:
+                plt.plot([pX[i][it-1][0], pX[i][it][0]], [pX[i][it-1][1], pX[i][it][1]], c=c, linestyle='--', linewidth=1)
+
+plt.title('PSO Particle Path')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.grid(True)
+print(f"save to plot/{func_str_table[fn]}/{func_str_table[fn]}_spso_plot.png")
+plt.savefig(f'plot/{func_str_table[fn]}/{func_str_table[fn]}_spso_plot.png')
+plt.close()
+
+plt.figure()
+plt.xlabel('iteration')
+plt.ylabel('value')
+iteration = [i for i in range(k)]
+plt.plot(iteration, gbest)
+print(f"save to plot/{func_str_table[fn]}/{func_str_table[fn]}_spso_convergence.png")
+plt.savefig(f'plot/{func_str_table[fn]}/{func_str_table[fn]}_spso_convergence.png')
+plt.close()
+
+fig, ax = plt.subplots()
+ax.contour(X, Y, Z)
+ax.set_xlim(search_range[fn][0], search_range[fn][1])
+ax.set_ylim(search_range[fn][0], search_range[fn][1])
+
+points = []
+for i in range(10):
+    points.append(ax.scatter(pX[i][0][0], pX[i][0][1]))
+
+def update_scatter(frame):
+    it = frame
+    ax.set_title('iter = ' + str(it))
+    for i in range(10):
+        points[i].set_offsets((pX[i][it][0], pX[i][it][1]))
+    return tuple(points)
+
+ani = animation.FuncAnimation(fig, update_scatter, frames=k, interval=200)
+writer = animation.PillowWriter(fps=15,
+                                metadata=dict(artist='Me'),
+                                bitrate=1800)
+
+print(f"save to plot/{func_str_table[fn]}/{func_str_table[fn]}_spso_path.gif")
+ani.save(f'plot/{func_str_table[fn]}/{func_str_table[fn]}_spso_path.gif', writer=writer)
