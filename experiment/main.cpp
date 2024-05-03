6620b0d258697fb95a4d2e6537e125bd3107c61c
OEmiliatanO <emiliaistruelove@gmail.com>
2024-05-03 10:50:54 +0800

    experimental algorithm

---
+++ experiment/main.cpp
@@ -0,0 +1,241 @@
+#include <iostream>
+#include <format>
+#include <fstream>
+#include <cmath>
+#include <chrono>
+#include <random>
+#include <deque>
+#include <queue>
+
+#include <vector.h>
+#include <test_function.h>
+
+double (*test_func)(const CH::vector<double>& X);
+
+std::string table[] = {"0", "Ackley", "Rastrigin", "HappyCat", "Rosenbrock", "Zakharov", "Michalewicz"};
+
+struct area_t
+{
+    std::vector<double> st;
+    std::vector<double> ed;
+    double f;
+};
+
+struct indv_t
+{
+    CH::vector<double> p, step;
+    double f;
+};
+
+bool operator<(const area_t& lhs, const area_t& rhs)
+{
+    return lhs.f > rhs.f;
+}
+bool operator<(const indv_t& lhs, const indv_t& rhs)
+{
+    return lhs.f < rhs.f;
+}
+
+std::vector<indv_t> split(const CH::vector<double>& st, const CH::vector<double>& ed, size_t n, size_t m)
+{
+    std::random_device rnd_device;
+    std::mt19937 eng { rnd_device() };
+    std::uniform_int_distribution<size_t> dist {0, n-1};
+
+    std::vector<indv_t> vec;
+    vec.resize(n);
+    auto d = st.size();
+    if (d != ed.size())
+    {
+        std::cerr << "st.size() != ed.size()" << std::endl;
+        exit(0);
+    }
+
+    for (size_t i = 0; i < n; ++i)
+    {
+        vec[i].p.resize(d);
+        vec[i].step.resize(d);
+    }
+
+    for (size_t i = 0; i < d; ++i)
+    {
+        double step = (ed[i] - st[i]) / m;
+        for (size_t j = 0; j < n; ++j)
+        {
+            auto k = dist(eng);
+            vec[j].step[i] = (ed[i] - st[i]) / m;
+            vec[j].p[i] = ((st[i] + k * step) + (st[i] + (k+1) * step)) / 2;
+        }
+    }
+
+    return vec;
+}
+
+area_t neighbor(const indv_t& center)
+{
+    area_t area;
+    area.st.resize(center.p.size());
+    area.ed.resize(center.p.size());
+    for (size_t i = 0; i < center.p.size(); ++i)
+    {
+        area.st[i] = center.p[i] - center.step[i] / 2;
+        area.ed[i] = center.p[i] + center.step[i] / 2;
+    }
+    area.f = center.f;
+    return area;
+}
+
+double SA(size_t N, size_t D, size_t max_eval, [[maybe_unused]]double m, [[maybe_unused]]double c, double xl, double xu, [[maybe_unused]] int fn = 1)
+{
+#ifdef SAVE
+    std::fstream fs;
+    fs.open(std::format("plot/{}/{}_path.txt", table[fn], table[fn]), std::ios::out);
+    //fs << std::format("{} {} {}", N, D, max_eval) << std::endl;
+#endif
+
+    std::priority_queue<area_t> qu;
+    CH::vector<double> gbest;
+    double fgbest;
+
+    /*  init  */
+    {
+        area_t tmp_area{};
+        
+        for (size_t i = 0; i < D; ++i)
+        {
+            tmp_area.st.emplace_back(xl);
+            tmp_area.ed.emplace_back(xu);
+        }
+        qu.emplace(tmp_area);
+    }
+    std::random_device rnd_device;
+    std::mt19937 eng { rnd_device() };
+    std::uniform_real_distribution<double> Xdist {xl, xu};
+    std::uniform_real_distribution<double> dist01 {0, 1};
+
+    fgbest = std::numeric_limits<double>::max();
+    /*  init  */
+
+    /*  iteration */
+    size_t it = 0, cnt = 0;
+    while (qu.size() and cnt < max_eval)
+    {
+        auto new_p = split(qu.top().st, qu.top().ed, N, m); qu.pop();
+
+        for (size_t i = 0; i < new_p.size(); ++i)
+        {
+            new_p[i].f = test_func(new_p[i].p);
+            ++cnt;
+            if (cnt >= max_eval)
+                new_p[i].f = std::numeric_limits<double>::max();
+        }
+        
+        std::sort(new_p.begin(), new_p.end());
+        
+        if (fgbest > new_p[0].f)
+        {
+            gbest = new_p[0].p;
+            fgbest = new_p[0].f;
+#ifdef SAVE
+            fs << new_p.size() << std::endl;
+            for (size_t i = 0; i < new_p.size(); ++i)
+                fs << new_p[i].p << '\n' << new_p[i].step << std::endl;
+            fs << fgbest << std::endl;
+#endif
+        }
+
+        for (size_t i = 0; i < std::min(new_p.size(), N); ++i)
+            qu.emplace(neighbor(new_p[i]));
+
+        //std::cerr << std::format("iteration #{}: global best fitness = {}", it, fgbest) << std::endl;
+
+        ++it;
+    }
+    /*  iteration */
+
+#ifdef SAVE
+    fs.close();
+#endif
+    std::cout << fgbest << std::endl;
+    return fgbest;
+}
+
+// --N particle_num
+// --D dimension
+// --k iteration_num
+// --test_function fn
+// (k+1)*N <= D*10^4
+// ./main.elf --N 10 --D 10 --k 1 --m 1 --c 0.5 --test_function Ackley
+int main([[maybe_unused]]int argc, char **argv)
+{
+    using namespace std::chrono_literals;
+
+    int N, D, k, fn, m;
+    double c;
+
+    sscanf(argv[2], "%d", &N);
+    sscanf(argv[4], "%d", &D);
+    sscanf(argv[6], "%d", &k);
+    sscanf(argv[8], "%d", &m);
+    sscanf(argv[10], "%lf", &c);
+    sscanf(argv[12], "%d", &fn);
+
+    switch(fn)
+    {
+        case 1:
+            test_func = static_cast<double (*)(const CH::vector<double>&)>(&Ackley);
+            break;
+
+        case 2:
+            test_func = static_cast<double (*)(const CH::vector<double>&)>(&Rastrigin);
+            break;
+
+        case 3:
+            test_func = static_cast<double (*)(const CH::vector<double>&)>(HappyCat);
+            break;
+
+        case 4:
+            test_func = static_cast<double (*)(const CH::vector<double>&)>(Rosenbrock);
+            break;
+
+        case 5:
+            test_func = static_cast<double (*)(const CH::vector<double>&)>(Zakharov);
+            break;
+
+        case 6:
+            test_func = static_cast<double (*)(const CH::vector<double>&)>(Michalewicz);
+            break;
+    }
+
+    k = D * 10000;
+    std::cerr << std::format("N = {}, D = {}, k = {}", N, D, k) << std::endl;
+
+    //constexpr auto EVAL_N = 30;
+    auto eval_num = EVAL_N;
+    double mean = 0, std = 0;
+    std::chrono::duration<double> time_mean = 0ms;
+    while (eval_num--)
+    {
+        double xl = 0, xu = 30;
+        set_search_bound(&xu, &xl, fn);
+        auto st = std::chrono::steady_clock::now();
+        auto tmp_res = SA(N, D, k, m, c, xl, xu, fn);
+        auto ed = std::chrono::steady_clock::now();
+        time_mean += ed - st;
+
+        mean += tmp_res;
+        std += tmp_res * tmp_res;
+    }
+
+    mean /= EVAL_N;
+    time_mean /= EVAL_N;
+    std /= EVAL_N;
+    std -= mean * mean;
+    if (std::abs(std) < 1e-6) std = 0;
+    std = sqrt(std);
+    
+    //std::cout << std::format("mean: {:.4f}±{:.4f}", mean, std) << std::endl;
+
+    std::cerr << std::format("mean: {}±{}, avg time: {}", mean, std, time_mean) << std::endl;
+    return 0;
+}
---
+++ experiment/plot.py
@@ -0,0 +1,91 @@
+import numpy as np
+from random import randint, random
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from src import test_function as tfn
+import sys
+
+func_str_table = {1:"Ackley", 2:"Rastrigin", 3:"HappyCat", 4:"Rosenbrock", 5:"Zakharov", 6:"Michalewicz"}
+search_range = {1:[-32.768, 32.768], 2:[-5.12, 5.12], 3:[-20.0, 20.0], 4:[-10.0, 10.0], 5:[-10.0, 10.0], 6:[0.0, np.pi]}
+
+fn = int(sys.argv[1])
+x = np.linspace(search_range[fn][0], search_range[fn][1], 100)
+y = np.linspace(search_range[fn][0], search_range[fn][1], 100)
+X, Y = np.meshgrid(x, y)
+Z = []
+for i in range(X.shape[0]):
+    for j in range(X.shape[1]):
+        Z.append(tfn.function_table[fn]([X[i, j],Y[i,j]]))
+Z = np.array(Z)
+Z = Z.reshape((X.shape[0], X.shape[1]))
+
+plt.figure()
+contour = plt.contour(X, Y, Z)
+#plt.clabel(contour, inline = True)
+plt.colorbar(contour, label=f'{func_str_table[fn]} Function Value')
+
+with open(f"plot/{func_str_table[fn]}/{func_str_table[fn]}_path.txt") as f:
+    pX = []
+    step = []
+    gbest = []
+    while True:
+        k = f.readline()
+        if k is None or k == '':
+            break
+        k = int(k.strip())
+        pX.append([])
+        step.append([])
+        for j in range(k):
+            pX[-1].append([float(x.strip()) for x in f.readline().strip().split(' ')])
+            step[-1].append([float(x.strip())/2 for x in f.readline().strip().split(' ')])
+        gbest.append(float(f.readline().strip()))
+
+plt.figure()
+plt.title('DE')
+plt.xlabel('iteration')
+plt.ylabel('value')
+iteration = [i for i in range(len(pX))]
+plt.plot(iteration, gbest)
+print(f"save to plot/{func_str_table[fn]}/{func_str_table[fn]}_de_convergence.png")
+plt.savefig(f'plot/{func_str_table[fn]}/{func_str_table[fn]}_de_convergence.png')
+plt.close()
+
+fig, ax = plt.subplots()
+ax.contour(X, Y, Z)
+ax.set_xlim(search_range[fn][0], search_range[fn][1])
+ax.set_ylim(search_range[fn][0], search_range[fn][1])
+
+points = []
+borders = []
+
+dirr = [[-1,-1,-1,+1], [-1,+1,+1,+1], [+1,+1,+1,-1], [+1,-1,-1,-1]]
+
+for i in range(len(pX[0])):
+    points.append(ax.scatter(pX[0][i][0], pX[0][i][1]))
+    cx, cy = pX[0][i][0], pX[0][i][1]
+    for j in range(4):
+        a, b = pX[0][i][0] + dirr[j][0] * step[0][i][0], pX[0][i][1] + dirr[j][1] * step[0][i][1]
+        c, d = pX[0][i][0] + dirr[j][2] * step[0][i][0], pX[0][i][1] + dirr[j][3] * step[0][i][1]
+        line, = ax.plot([a, c], [b, d], c="red", linewidth=1)
+        borders.append(line)
+
+def update_scatter(frame):
+    it = frame
+    ax.set_title('iter = ' + str(it))
+    for i in range(len(pX[it])):
+        points[i].set_offsets((pX[it][i][0], pX[it][i][1]))
+        for j in range(4):
+            a, b = pX[it][i][0] + dirr[j][0] * step[it][i][0], pX[it][i][1] + dirr[j][1] * step[it][i][1]
+            c, d = pX[it][i][0] + dirr[j][2] * step[it][i][0], pX[it][i][1] + dirr[j][3] * step[it][i][1]
+            borders[i*4+j].set_xdata([a, c])
+            borders[i*4+j].set_ydata([b, d])
+            
+    return tuple(points)
+
+ani = animation.FuncAnimation(fig, update_scatter, frames=len(pX), interval=200)
+writer = animation.PillowWriter(fps=15,
+                                metadata=dict(artist='Me'),
+                                bitrate=1800)
+
+print(f"save to plot/{func_str_table[fn]}/{func_str_table[fn]}_de_path.gif")
+ani.save(f'plot/{func_str_table[fn]}/{func_str_table[fn]}_de_path.gif', writer=writer)
