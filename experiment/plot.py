68b625585e9903f2b1f99323c79efc6c1c60f823
OEmiliatanO <emiliaistruelove@gmail.com>
2024-05-03 10:56:49 +0800

    replot

--- experiment/plot.py
+++ experiment/plot.py
@@ -82,7 +82,7 @@ def update_scatter(frame):
             
     return tuple(points)
 
-ani = animation.FuncAnimation(fig, update_scatter, frames=len(pX), interval=800)
+ani = animation.FuncAnimation(fig, update_scatter, frames=len(pX), interval=1000)
 writer = animation.PillowWriter(fps=1,
                                 metadata=dict(artist='Me'),
                                 bitrate=1800)
